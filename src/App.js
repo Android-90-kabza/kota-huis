6bebbf27528ac3e587eb5f237cc3f890349fdd6d
ntshoe20 <itumeleng.ntshoe@gmail.com>
2021-08-07 23:13:12 +0200

    updated files with structure and render new hello section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 
 import React from 'react';
-import Header from "./components/Header";
+import Login from "./components/Login";
 import './App.css';
 
 function App() {
   return (
-<Header/>
+    <Login/>
   );
 }
 
