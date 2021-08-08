b65540514e9137ce9f8d4fc632911089c485a9ab
ntshoe20 <itumeleng.ntshoe@gmail.com>
2021-08-08 10:05:36 +0200

    created login section

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,109 @@
 import React from 'react';
+import Container from '@material-ui/core/Container';
+import { makeStyles,createStyles,Theme,useTheme } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import { green } from '@material-ui/core/colors';
+import Box from '@material-ui/core/Box';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import SwipeableViews from 'react-swipeable-views';
 
-class Login extends React.Component {
-    render() {
-      return (
-        <h1>Login</h1>
-      );
-    }
-  }
-  export default Login;
+interface TabPanelProps {
+  children?: React.ReactNode;
+  dir?: string;
+  index: any;
+  value: any;
+}
+
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <Typography
+      component="div"
+      role="tabpanel"
+      hidden={value !== index}
+      id={`action-tabpanel-${index}`}
+      aria-labelledby={`action-tab-${index}`}
+      {...other}
+    >
+      {value === index && <Box p={3}>{children}</Box>}
+    </Typography>
+  );
+}
+function a11yProps(index: any) {
+  return {
+    id: `action-tab-${index}`,
+    'aria-controls': `action-tabpanel-${index}`,
+  };
+}
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      backgroundColor: theme.palette.background.paper,
+      width: 500,
+      position: 'relative',
+      minHeight: 200,
+    },
+    fab: {
+      position: 'absolute',
+      bottom: theme.spacing(2),
+      right: theme.spacing(2),
+    },
+    fabGreen: {
+      color: theme.palette.common.white,
+      backgroundColor: green[500],
+      '&:hover': {
+        backgroundColor: green[600],
+      },
+    },
+  }),
+);
+
+export default function Login() {
+  const classes = useStyles();
+  const theme = useTheme();
+  const [value, setValue] = React.useState(0)
+
+  const handleChange = (event: unknown, newValue: number) => {
+    setValue(newValue);
+  };
+
+  const handleChangeIndex = (index: number) => {
+    setValue(index);
+  };
+
+  return (
+    <Container maxWidth="sm">
+        <div className={classes.root}>
+      <AppBar position="static" color="default">
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          indicatorColor="primary"
+          textColor="primary"
+          variant="fullWidth"
+          aria-label="action tabs example"
+        >
+          <Tab label="Login" {...a11yProps(0)} />
+          <Tab label="Sign up" {...a11yProps(1)} />
+        </Tabs>
+      </AppBar>
+      <SwipeableViews
+        axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+        index={value}
+        onChangeIndex={handleChangeIndex}
+      >
+        <TabPanel value={value} index={0} dir={theme.direction}>
+          Login form
+        </TabPanel>
+        <TabPanel value={value} index={1} dir={theme.direction}>
+          Sign up Form
+        </TabPanel>
+      </SwipeableViews>
+    </div>
+    </Container>
+  );
+}
